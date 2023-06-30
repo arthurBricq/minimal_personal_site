1d47282c79d49d0453c9247e7ca5cc0f139b54ec
Arthur Bricq <arthur.bricq@bluebotics.com>
2023-06-30 16:32:24 +0200

    Refactor header parsing.
    Now, the headers are also statically created which makes it much easier
    to find the correct link.

--- generate_site.py
+++ generate_site.py
@@ -8,18 +8,32 @@ from distutils.dir_util import copy_tree
 import shutil
 import os
 
+
 def clear_folder(path):
     """
     Deletes a folder, then create it again.
     """
     # deletetion
-    try: 
+    try:
         shutil.rmtree(path)
-        print(f"Folder {path} was succesfully deleted")
+        print(f"Folder {path} was successfully deleted")
     except OSError as e:
         print(f"Could not delete folder {path}: {e}")
     # creation
     os.makedirs(path)
+
+def get_header(root = ".") -> str:
+    return f"""
+<header>
+    Arthur Bricq
+</header>
+
+<nav>
+    <a href="{root}/writings.html">Writing</a>
+    <a href="{root}/projects.html">Projects</a>
+    <a href="{root}/index.html">About</a>
+</nav>
+    """
 
 
 class PageBuilder:
@@ -35,18 +49,20 @@ class PageBuilder:
         * template: template for the children page
         * children_output_path: where to write the generated pages
         """
-        self._editor = html_editor
-        self._template_path = template
         self._root = site_root
         self._name = page_name
+        self._editor = html_editor
+        self._template_path = template
         clear_folder(site_root + page_name)
 
     def add_children(self, _meta, _soup):
         """
         Add a project to the page's content
+        * meta contains information about the project
+        * soup contains the parsed 
         """
         # create html div with a link to the project's page.
-        tag = self.create_html_link(_meta, _soup)
+        tag = self.create_html_link(_meta)
 
         # Modify the project page
         self._editor.append(tag)
@@ -55,47 +71,69 @@ class PageBuilder:
         self._create_project_page(_meta, _soup)
 
     def _create_project_page(self, _meta, _soup):
-        # Load the template
+        # Load the template for children pages
         with Path(self._template_path).open() as data:
             html_text = data.read()
         if html_text is None:
             print("ERROR opening project's page html template")
-        # Fill the template
-        template = bs4.BeautifulSoup(html_text, 'html.parser')
+
+        # Find where to add information
+        template = bs4.BeautifulSoup(html_text, "html.parser")
         box = template.find(id="content")
+        if box is None: return
+
+        # Fill the template with the provided html
         box.append(_soup)
 
+        # Fill the header
+        header_container = template.find(id = "header_container")
+        if header_container is None: return
+        parsed = bs4.BeautifulSoup(get_header(root = ".."), "html.parser")
+        header_container.append(parsed)
+
         # Save the file
-        title = _meta['title']
-        with open(self._root + self._name + f"/{title}.html", "w") as outf:
-            outf.write(str(template))
+        title = _meta["title"]
+        with open(self._root + self._name + f"/{title}.html", "w") as file:
+            file.write(str(template))
 
-    # Functions to be overriden
-    def create_html_link(self, _meta, _soup):
-        title = _meta['title']
-        html_list = ''
-        html_list += '<li class="project_item"><a href="./{}/{}.html">{}</a> </li>'.format(self._name, title, title);
-        if ('github' in _meta):
-            html_list += '<p class="description">Link to <a href="{}">Github</a></p>'.format(_meta['github']);
-        if ('keywords' in _meta):
-            html_list += '<p class="keyword">keywords: {}</p>'.format(_meta['keywords']);
-        html_list += '<p class="description">{}</p>'.format(_meta['description']);
-        tag = bs4.BeautifulSoup(html_list, 'html.parser')
-        return tag;
+    def create_html_link(self, _meta):
+        title = _meta["title"]
+        html_list = ""
+        html_list += (
+            '<li class="project_item"><a href="./{}/{}.html">{}</a> </li>'.format(
+                self._name, title, title
+            )
+        )
+        if "github" in _meta:
+            html_list += (
+                '<p class="description">Link to <a href="{}">Github</a></p>'.format(
+                    _meta["github"]
+                )
+            )
+        if "keywords" in _meta:
+            html_list += '<p class="keyword">keywords: {}</p>'.format(_meta["keywords"])
+        html_list += '<p class="description">{}</p>'.format(_meta["description"])
+        tag = bs4.BeautifulSoup(html_list, "html.parser")
+        return tag
 
-def create_index_page(template_path, content_path, output_path, container_id = "markdown_container"):
+
+
+def create_index_page(
+    template_path, content_path, output_path, container_id="markdown_container"
+):
     """
-    Creates the index.html page b y filling the template path from the content found in .md file
+    Creates the index.html page by filling the template path from the content found in .md file
     """
-    # read the template 
+    # read the template
     with Path(template_path).open() as data:
         html_text = data.read()
     if html_text is None:
         print("ERROR opening project html")
 
     # create container with the html elements
-    soup = bs4.BeautifulSoup(html_text, 'html.parser')
-    container = soup.find(id=container_id)
+    soup = bs4.BeautifulSoup(html_text, "html.parser")
+    text_container = soup.find(id=container_id)
+    if text_container is None: return
 
     # read the content
     with Path(content_path).open() as data:
@@ -105,34 +143,42 @@ def create_index_page(template_path, content_path, output_path, container_id = "
 
     # parse and add the markdown
     html = markdown2.markdown(content)
-    parsed = bs4.BeautifulSoup(html, 'html.parser')
-    container.append(parsed)
+    parsed = bs4.BeautifulSoup(html, "html.parser")
+    text_container.append(parsed)
+
+    # fill the header
+    header_container = soup.find(id = "header_container")
+    if header_container is None: return
+    parsed = bs4.BeautifulSoup(get_header(), "html.parser")
+    header_container.append(parsed)
 
     # save the output
-    with open(output_path, "w") as outf:
-        outf.write(str(soup))
+    with open(output_path, "w") as file:
+        file.write(str(soup))
 
-def create_project_page(site_root, page_name, page_template, children_template, 
-                        page_container_id, children_container_id,
-                        children_input_path 
-                        ):
+
+def create_page_with_children(
+    site_root,
+    page_name,
+    page_template,
+    children_template,
+    page_container_id,
+    children_input_path,
+):
     """
-    Create the project page.
-
-    Inputs
-    ------
-    * TODO
-
+    Creates a page with associated children pages, listed under this page.
     """
-    # read the project 
+    # read the project
     with Path(page_template).open() as data:
         html_text = data.read()
     if html_text is None:
         print("ERROR opening project html")
 
     # create container with the html elements
-    soup = bs4.BeautifulSoup(html_text, 'html.parser')
-    project_page = PageBuilder(site_root, page_name, soup.find(id=page_container_id), children_template)
+    soup = bs4.BeautifulSoup(html_text, "html.parser")
+    project_page = PageBuilder(
+        site_root, page_name, soup.find(id=page_container_id), children_template
+    )
 
     # for all projects with a markdown description, add the project to the page's content.
     project_folder = Path(children_input_path).glob("**/*.md")
@@ -146,32 +192,47 @@ def create_project_page(site_root, page_name, page_template, children_template,
             print("ERROR opening one of the project: ", p)
 
         # create html content
-        html = markdown2.markdown(md, extras=['metadata'])
+        html = markdown2.markdown(md, extras=["metadata"])
         meta = html.metadata
         projects.append({"html": html, "meta": meta})
 
-    # sort the list of projects
-    projects = sorted(projects, key= lambda element: element['meta']['priority'])
+    # sort the list of projects and add them to the parent page
+    projects = sorted(projects, key=lambda element: element["meta"]["priority"])
     for p in projects:
-        parsed = bs4.BeautifulSoup(p['html'], 'html.parser')
-        project_page.add_children(p['meta'], parsed)
+        parsed = bs4.BeautifulSoup(p["html"], "html.parser")
+        project_page.add_children(p["meta"], parsed)
+
+    # fill the header
+    header_container = soup.find(id = "header_container")
+    if header_container is None: return
+    parsed = bs4.BeautifulSoup(get_header(), "html.parser")
+    header_container.append(parsed)
 
     # Finally, save the project page
-    with open(site_root + page_name + '.html', "w") as outf:
-        outf.write(str(soup))
+    with open(site_root + page_name + ".html", "w") as file:
+        file.write(str(soup))
+
 
 if __name__ == "__main__":
     # create project
-    create_project_page(site_root = 'outsite/', page_name = 'projects',
-                        page_template = './src/projects.html', children_template = './src/children_template.html', 
-                        page_container_id = 'projects_list', children_container_id = 'content', 
-                        children_input_path = './src/data/projects')
+    create_page_with_children(
+        site_root="outsite/",
+        page_name="projects",
+        page_template="./src/projects.html",
+        children_template="./src/children_template.html",
+        page_container_id="projects_list",
+        children_input_path="./src/data/projects",
+    )
 
     # create writing page
-    create_project_page(site_root = 'outsite/', page_name = 'writings',
-                        page_template = './src/writing.html', children_template = './src/children_template.html', 
-                        page_container_id = 'article_list', children_container_id = 'content', 
-                        children_input_path = './src/data/writing')
+    create_page_with_children(
+        site_root="outsite/",
+        page_name="writings",
+        page_template="./src/writing.html",
+        children_template="./src/children_template.html",
+        page_container_id="article_list",
+        children_input_path="./src/data/writing",
+    )
 
     # create main page
     create_index_page("src/index.html", "src/data/text.md", "outsite/index.html")
